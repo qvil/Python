67d24a0463f8760e23a8abda2040ec7bea7548fc
qvil <qvil@naver.com>
2015-05-20 15:42:55 +0900

    Add Solution travel.py

--- Homework/travel.py
+++ Homework/travel.py
@@ -1,8 +1,17 @@
-a = [5]
+##############################################
+# Date : 150520 Tue
+# Author : Taesu Hyeon
+# Travel Place Random Choice Program
+##############################################
+import random
+
+place = [5]
 cnt = 0
-for i in range(0,4):
-	inputval = input("Input travel place : ")
-	a[cnt] = inputval
-	a.append(inputval)
+
+for i in range(0,5):
+	inputval = raw_input("Input travel place : ")
+	place[cnt] = inputval
+	place.append(inputval)
 	cnt += 1
-print(a)
+
+print("travel place : %s" % (random.choice(place)))
