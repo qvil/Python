8c27c8338b5d4ea5b9ff023bc08dc222a615a7a4
taesu <qvil@naver.com>
2015-05-29 22:22:27 +0900

    add gugudan

---
+++ Class/9week(error,bmi)/20150529gugudan.py
@@ -0,0 +1,5 @@
+for index1 in range(2,10):
+	for index2 in range(1,10):
+		result = index1 * index2
+
+		print("%d * %d = %d" % (index1,index2,result))
--- Class/9week(error,bmi)/error.py
+++ Class/9week(error,bmi)/error.py
@@ -1,10 +1,12 @@
 import sys
 
 try:
-    a = float(sys.argv[1])
-    b = float(sys.argv[2])
-    print(a+b)
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
+    c = int(sys.argv[3])
+    print(a+b-c)
 except:
     print('Error!')
 
 print('End')
+
