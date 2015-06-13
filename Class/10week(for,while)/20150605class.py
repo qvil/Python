0ee49ffb2b41988dbe761db0a9c3e45043e8d717
soonman <soooon1001@gmail.com>
2015-06-13 10:35:12 +0900

    I do it

---
+++ Class/10week(for,while)/20150605class.py
@@ -0,0 +1,19 @@
+# milk = 1000
+
+# for index in range(1,10):
+# 	print(milk)
+# 	print(index)
+
+# a = 'soonman'
+
+
+
+
+# for index in range(1,5):
+# 	for index2 in range(1,10):
+# 		print("name : %d num : %d" % (index, index2))
+
+
+for index in range(2,10):
+	for index2 in range(1,10):
+		print("%d x %d = %d" % (index,index2,(index*index2)))
---
+++ Class/11week(file)/20150605homework(inputgugudan).py
@@ -0,0 +1,14 @@
+# var = input('what number?=')
+# for index in range(2,10):
+	# print ("%d x %d = %d" % (var,index,(var*index))
+
+
+
+
+
+
+########################################################
+
+var = input('what number? ')
+for index in range(1,10):
+	print ("%d x %d = %d" % (var,index,(var*index)))
