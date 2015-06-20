4324caf6ae0bb8dd3a3242295b26ef52b387b7ed
soonman <soooon1001@gmail.com>
2015-06-20 10:49:17 +0900

    newfile.txt, turtle update

---
+++ Project/turtle_test-150620.py
@@ -0,0 +1,33 @@
+import turtle
+import time
+geobuk = turtle.Turtle()
+geobuk.shape('turtle')
+geobuk.shapesize(4,4,5)
+geobuk.penup()
+geobuk.forward(250)
+geobuk.right(90)
+geobuk.pendown()	
+geobuk.forward(50)
+geobuk.left(90)
+geobuk.backward(250)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+time.sleep(6)
