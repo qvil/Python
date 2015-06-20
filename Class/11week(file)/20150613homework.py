53b48c76d851fa533a1249e31b584e3cc1dfbf79
soonman <soooon1001@gmail.com>
2015-06-20 10:48:10 +0900

    Add homework

--- Class/11week(file)/20150613homework.py
+++ Class/11week(file)/20150613homework.py
@@ -1,7 +1,33 @@
 f = open("newfile.txt", "r")
 read = f.readlines()
+length = len(read)
+for i in range(0,length):
+	if read[i] == "soonman":
+		print("soonman line : %d" % (i+1))
+	# else:
+		# print("nothing")
+
+	# print(read[i])
+
+
+
+
+
+
+
+
+
+
+
+
 # for index in len():
-print(read[0:])
+
+# print(read[0])
+# print(read[1])
+# print(length)
+
+# for i in range(0,length):
+# 	print(read[i])
 
 
 # print(read[])
@@ -15,4 +41,3 @@ print(read[0:])
 # print(read[1])
 # print(read[2])
 # print(read[3])
-
