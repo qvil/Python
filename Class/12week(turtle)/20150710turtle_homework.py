88ba09be2186c726f1a411ff89c5b75116a92b83
soonman <soooon1001@gmail.com>
2015-07-16 23:12:47 +0900

    1.homework clear

---
+++ Class/12week(turtle)/20150710turtle_homework.py
@@ -0,0 +1,22 @@
+import turtle
+
+forward = input("Forward? : ")
+left = input("Left? : ")
+
+g = turtle.Turtle()
+
+g.home
+g.clear
+g.forward(forward)
+g.left(left)
+
+
+###########################################################
+
+# import turtle
+
+# g = turtle.Turtle()
+
+# g.home
+# g.clear
+# g.forward(forward = input("Forward? : "))
